b75d7efd44493a8b5c6508067533e6ab3da84432
dokar3 <68095777+dokar3@users.noreply.github.com>
2023-07-10 18:53:52 +0800

    Make app nav graph cleaner

--- app/src/main/java/com/dokar/upnextgpt/AppNavGraph.kt
+++ app/src/main/java/com/dokar/upnextgpt/AppNavGraph.kt
@@ -1,5 +1,8 @@
 package com.dokar.upnextgpt
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -9,6 +12,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,6 +23,51 @@ import io.upnextgpt.ui.home.QueueScreen
 import io.upnextgpt.ui.home.viewmodel.HomeViewModel
 import io.upnextgpt.ui.settings.SettingsScreen
 import org.koin.androidx.compose.koinViewModel
+
+private typealias TransitionScope = AnimatedContentTransitionScope<NavBackStackEntry>
+
+private data class ScreenTransitions(
+    val enter: TransitionScope.() -> EnterTransition,
+    val exit: TransitionScope.() -> ExitTransition,
+)
+
+private val HomeScreenTransitions = ScreenTransitions(
+    enter = {
+        scaleIn(
+            initialScale = 0.8f,
+            animationSpec = tween(durationMillis = 300),
+        ) + fadeIn(
+            animationSpec = tween(durationMillis = 300),
+        )
+    },
+    exit = {
+        scaleOut(
+            targetScale = 0.8f,
+            animationSpec = tween(durationMillis = 225),
+        ) + fadeOut(
+            animationSpec = tween(durationMillis = 225),
+        )
+    },
+)
+
+private val SubScreenTransitions = ScreenTransitions(
+    enter = {
+        scaleIn(
+            initialScale = 1.2f,
+            animationSpec = tween(durationMillis = 300),
+        ) + fadeIn(
+            animationSpec = tween(durationMillis = 300),
+        )
+    },
+    exit = {
+        scaleOut(
+            targetScale = 1.2f,
+            animationSpec = tween(durationMillis = 225),
+        ) + fadeOut(
+            animationSpec = tween(durationMillis = 225),
+        )
+    },
+)
 
 @Composable
 fun AppNavGraph(
@@ -30,78 +81,65 @@ fun AppNavGraph(
             navController = navController,
             startDestination = "home",
         ) {
-            composable(
-                route = "home",
-                exitTransition = {
-                    scaleOut(
-                        targetScale = 0.8f,
-                        animationSpec = tween(durationMillis = 225),
-                    ) + fadeOut(
-                        animationSpec = tween(durationMillis = 225),
-                    )
-                },
-                popEnterTransition = {
-                    scaleIn(
-                        initialScale = 0.8f,
-                        animationSpec = tween(durationMillis = 300),
-                    ) + fadeIn(
-                        animationSpec = tween(durationMillis = 300),
-                    )
-                },
-            ) {
-                HomeScreen(
-                    viewModel = homeViewModel,
-                    onNavigate = { navController.navigate(it) },
-                )
-            }
+            homeScreen(
+                navController = navController,
+                homeViewModel = homeViewModel,
+            )
 
-            composable(
-                route = "queue",
-                enterTransition = {
-                    scaleIn(
-                        initialScale = 1.2f,
-                        animationSpec = tween(durationMillis = 300),
-                    ) + fadeIn(
-                        animationSpec = tween(durationMillis = 300),
-                    )
-                },
-                exitTransition = {
-                    scaleOut(
-                        targetScale = 1.2f,
-                        animationSpec = tween(durationMillis = 225),
-                    ) + fadeOut(
-                        animationSpec = tween(durationMillis = 225),
-                    )
-                },
-            ) {
-                QueueScreen(
-                    viewModel = homeViewModel,
-                    onBack = { navController.popBackStack() },
-                )
-            }
-            composable(
-                route = "settings",
-                enterTransition = {
-                    scaleIn(
-                        initialScale = 1.2f,
-                        animationSpec = tween(durationMillis = 300),
-                    ) + fadeIn(
-                        animationSpec = tween(durationMillis = 300),
-                    )
-                },
-                exitTransition = {
-                    scaleOut(
-                        targetScale = 1.2f,
-                        animationSpec = tween(durationMillis = 225),
-                    ) + fadeOut(
-                        animationSpec = tween(durationMillis = 225),
-                    )
-                },
-            ) {
-                SettingsScreen(
-                    onBack = { navController.popBackStack() },
-                )
-            }
+            queueScreen(
+                navController = navController,
+                homeViewModel = homeViewModel,
+            )
+
+            settingsScreen(
+                navController = navController,
+            )
         }
+    }
+}
+
+fun NavGraphBuilder.homeScreen(
+    navController: NavController,
+    homeViewModel: HomeViewModel,
+) {
+    composable(
+        route = "home",
+        exitTransition = HomeScreenTransitions.exit,
+        popEnterTransition = HomeScreenTransitions.enter,
+    ) {
+        HomeScreen(
+            viewModel = homeViewModel,
+            onNavigate = { navController.navigate(it) },
+        )
+    }
+}
+
+fun NavGraphBuilder.queueScreen(
+    navController: NavController,
+    homeViewModel: HomeViewModel,
+) {
+    composable(
+        route = "queue",
+        enterTransition = SubScreenTransitions.enter,
+        exitTransition = SubScreenTransitions.exit,
+    ) {
+        QueueScreen(
+            viewModel = homeViewModel,
+            onBack = { navController.popBackStack() },
+        )
+    }
+}
+
+fun NavGraphBuilder.settingsScreen(
+    navController: NavController,
+) {
+    composable(
+        route = "settings",
+        enterTransition = SubScreenTransitions.enter,
+        exitTransition = SubScreenTransitions.exit,
+    ) {
+        SettingsScreen(
+            onBack = { navController.popBackStack() },
+        )
     }
 }
