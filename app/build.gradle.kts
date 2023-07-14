b0c3eed87f911822b8889c1c84e81279835e2b0e
dokar3 <68095777+dokar3@users.noreply.github.com>
2023-07-14 20:41:51 +0800

    v1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.dokar.upnextgpt"
         minSdk = 21
         targetSdk = 33
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
