8c5b678e68e3d2dce887643f1d53f8c726be975c
dokar3 <68095777+dokar3@users.noreply.github.com>
2023-08-10 12:38:35 +0800

    Center vertically the dynamic color switch

--- ui/settings/src/main/java/io/upnextgpt/ui/settings/items/UiItem.kt
+++ ui/settings/src/main/java/io/upnextgpt/ui/settings/items/UiItem.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
@@ -39,6 +40,7 @@ internal fun UiItem(
                 },
             )
             .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
         Column(modifier = Modifier.weight(1f)) {
             Text(text = "Dynamic Color")
