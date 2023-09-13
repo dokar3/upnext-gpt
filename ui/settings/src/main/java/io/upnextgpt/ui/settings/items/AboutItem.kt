3d9df0274fba2074b1b5df627f6fc7695003b59b
dokar3 <68095777+dokar3@users.noreply.github.com>
2023-09-13 20:01:56 +0800

    Switch from HorizontalDivider to Divider

--- ui/settings/src/main/java/io/upnextgpt/ui/settings/items/AboutItem.kt
+++ ui/settings/src/main/java/io/upnextgpt/ui/settings/items/AboutItem.kt
@@ -13,7 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -75,7 +75,7 @@ internal fun AboutItem(
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        HorizontalDivider(
+        Divider(
             color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.24f)
         )
 
