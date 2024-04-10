0246d76c8a181bc5e5a345a1f3a2e252bc693ac2
dokar3 <68095777+dokar3@users.noreply.github.com>
2024-04-10 13:37:02 +0800

    Fix tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(platform(libs.compose.bom.alpha))
     androidTestImplementation(libs.compose.ui.test.junit4)
     debugImplementation(libs.compose.ui.tooling)
     debugImplementation(libs.compose.ui.test.manifest)
